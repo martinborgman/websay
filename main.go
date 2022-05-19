181db088fd442142a0ad1d84a9cf0e7c3974d4f6
Martin Borgman <borgman.martin@gmail.com>
2022-05-19 19:57:08 +0200

    Update dependencies to fix securiy issues

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
-	"github.com/bryanl/websay/pkg/say"
+	"github.com/martinborgman/websay/pkg/say"
 )
 
 func main() {
